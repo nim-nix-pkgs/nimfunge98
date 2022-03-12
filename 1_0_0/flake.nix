1fb21aa7564a9a1006fa71e76c45a2ae39bb4a9d
hugosenari <hugosenari@gmail.com>
2022-03-12 04:27:20 -0300

    chore: re index nimfunge98

---
+++ 1_0_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A Funge-98 interpreter written in nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimfunge98-1_0_0.flake = false;
+  inputs.src-nimfunge98-1_0_0.owner = "adyxax";
+  inputs.src-nimfunge98-1_0_0.ref   = "refs/tags/1.0.0";
+  inputs.src-nimfunge98-1_0_0.repo  = "nimfunge98";
+  inputs.src-nimfunge98-1_0_0.type  = "other";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimfunge98-1_0_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimfunge98-1_0_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ 1_0_1/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A Funge-98 interpreter written in nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimfunge98-1_0_1.flake = false;
+  inputs.src-nimfunge98-1_0_1.owner = "adyxax";
+  inputs.src-nimfunge98-1_0_1.ref   = "refs/tags/1.0.1";
+  inputs.src-nimfunge98-1_0_1.repo  = "nimfunge98";
+  inputs.src-nimfunge98-1_0_1.type  = "other";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimfunge98-1_0_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimfunge98-1_0_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
